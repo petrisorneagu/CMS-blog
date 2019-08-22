4097a0c61a24bef1bbc9d1b1d451e9aa6a9aba58
Petrisor Neagu <petrisor@madball.ro>
2019-08-22 18:20:52 +0300

    public admin profile for all users

---
+++ Profile.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet/less" type="text/css" href="css/style.css" />
+    <script src="https://kit.fontawesome.com/4966d9d9f9.js"></script>
+
+    <title>Profile</title>
+</head>
+<body>
+<!--navbar-->
+<div style="height:10px; background-color: rgba(62,81,180,0.7)"></div>
+<div class="navbar navbar-expand-lg navbar-dark bg-dark ">
+
+    <div class="container" >
+        <a href="#" class="navbar-brand">SARA.COM</a>
+        <button class="navbar-toggler" data-toggle="collapse" data-target="#navbarcollapseCMS">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarcollapseCMS">
+
+            <ul class="navbar-nav mr-auto">
+
+                <li class="nav-item">
+                    <a href="Blog.php" class="nav-link">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">About us</a>
+                </li>
+                <li class="nav-item">
+                    <a href="Blog.php" class="nav-link">Blog</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#$" class="nav-link">Contact us</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">Features</a>
+                </li>
+
+            </ul>
+
+
+            <ul class="navbar-nav ml-auto">
+                <form class="form-inline d-none d-sm-block" action="Blog.php">
+                    <div class="form-group">
+                        <input class="form-control mr-2" type="text" name="Search" placeholder="Search" value="">
+                        <button class="btn btn-primary" name="SearchButton">Go</button>
+                    </div>
+                </form>
+            </ul>
+        </div>
+    </div>
+</div>
+<div style="height:10px; background-color: rgba(62,81,180,0.7)"></div>
+
+<!--header-->
+<header class="bg-dark text-white py-3">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <h1> <i class="fas fa-user text-success mr-2"  style="color: rgba(62,81,180,0.7)"> </i> Name</h1>
+                <h3>Headline</h3>
+            </div>
+        </div>
+    </div>
+</header>
+<section class="container py-2 mb-4">
+    <div class="row">
+        <div class="col-md-3">
+            <img src="images/user.png" class="d-block img-fluid mb-3" alt="">
+        </div>
+        <div class="col-md-9" style="min-height: 600px">
+                <div class="card">
+                    <div class="card-body">
+                        <p class="lead">The value in a text field (and in a password field and in a hidden field, as well) is provided in its value attribute. However, the data there must be properly encoded with htmlspecialchars() to get rid of dangerous characters such as ' or < or >. The code snippet in the preceding code expects the form to be submitted back to itself; thus, it extracts the current value of the text field from $_POST (it would work analogously with $_GET).</p>
+                    </div>
+                </div>
+        </div>
+    </div>
+</section>
+<!--footer-->
+<div style="height:10px; background-color: rgba(62,81,180,0.7)"></div>
+
+<footer class="bg-dark text-white">
+
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <p class="lead text-center"> Theme by | Annonimous | <span id="year"></span> &copy; ---- All rights reserved.</p>
+            </div>
+        </div>
+    </div>
+</footer>
+<div style="height:10px; background-color: rgba(62,81,180,0.7)"></div>
+
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+
+<script>
+    $('#year').text(new Date().getFullYear());
+
+</script>
+</body>
+</html>
